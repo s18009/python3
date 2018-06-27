2c04c5fb8a3273ee6920f62cafe519aafc206313
s18009 <s18009@std.it-college.ac.jp>
2018-06-27 15:54:42 +0900

    HelloAmerica(修正)

--- hello_america.py
+++ hello_america.py
@@ -2,7 +2,12 @@ def func(home):
     if home == 'アメリカ':
         return 'Hello'
     return 'HelloWorld'
+
+
 def displey(s):
+
     print(s)
+
+
 if __name__ == '__main__':
     displey(func('日本'))
